7174f086b10100fbce4ef221f4a29bd7f6d4878c
Matthieu Riegler <kyro38@gmail.com>
2023-10-22 20:21:01 +0200

    optim

---
+++ projects/demo/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, isDevMode } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'demo';
+
+  constructor() {
+    isDevMode() ? console.log('devMode') : console.log('prodMode');
+  }
+}
